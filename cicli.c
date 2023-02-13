67625a8b423a3fe9fc8450ab773fb958ec636fe2
Andrea <andrea.giuliotti@iisviolamarchesini.edu.it>
2023-02-13 16:18:19 +0100

    esercizio cicli

---
+++ cicli.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int n1, n2;
+
+    do
+    {
+        printf("numero 1:");
+        scanf("%d", &n1);
+        if(n1==0)
+            break;
+        
+        printf("numero 2:");
+        if(n2==0)
+            break;
+
+        scanf("%d", &n2);
+        if (n1 > n2)
+        {
+            printf("%d", (n1 / n2));
+        }
+        else if (n2 > n1)
+        {
+            printf("%d", (n2 / n1));
+        }
+        else
+        {
+            printf("1");
+        }
+        printf("\n");
+
+    } while (1>0);
+}
